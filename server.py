c5a3de79b7edbcf9e076df8254e5e3ab21806efd
jaycode <teguhwpurwanto@gmail.com>
2016-03-17 01:14:01 +0700

    fix: Make port listening dynamic

--- server.py
+++ server.py
@@ -3,9 +3,10 @@ import tornado.ioloop
 import tornado.web
 import sockjs.tornado
 from app import app
+import config
 
 if __name__ == '__main__':
     # app.listen(os.getenv('VCAP_APP_PORT', 49152))
     # app.listen(49152)
-    app.listen(LISTENING_PORT)
+    app.listen(config.LISTENING_PORT)
     tornado.ioloop.IOLoop.instance().start()
