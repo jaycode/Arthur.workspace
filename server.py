d3172362d805dae631f6feaa57c9c2a12e19fcd1
jaycode <teguhwpurwanto@gmail.com>
2016-03-17 00:54:18 +0700

    fix: Try to use only port 49152

--- server.py
+++ server.py
@@ -5,5 +5,6 @@ import sockjs.tornado
 from app import app
 
 if __name__ == '__main__':
-    app.listen(os.getenv('VCAP_APP_PORT', 49152))
+    # app.listen(os.getenv('VCAP_APP_PORT', 49152))
+    app.listen(49152)
     tornado.ioloop.IOLoop.instance().start()
